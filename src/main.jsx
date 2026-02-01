67d47fe5c1d97b697d40ba0412c7c0246fb98683
maershaa <valeria.work2022@gmail.com>
2026-02-01 11:40:15 +0200

    fix: move GlobalStyle inside ThemeProvider to fix theme usage

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,10 @@ import { GlobalStyle } from './styles/global';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalStyle />
     <ThemeProvider theme={theme}>
+      {/* передаёт тему через props.theme во все styled-components внутри */}
+      <GlobalStyle />
+      {/* создаёт и подключает глобальные стили для всего приложения. */}
       <App />
     </ThemeProvider>
   </StrictMode>
